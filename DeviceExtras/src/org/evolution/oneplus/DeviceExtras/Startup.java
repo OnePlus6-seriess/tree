d71a8cd7c8533757223ae8eb53d01ca260c43e0b
Envoy-Z-Lab <envoyzlab@gmail.com>
2024-01-21 11:57:10 +0530

    uke

--- DeviceExtras/src/org/evolution/oneplus/DeviceExtras/Startup.java
+++ DeviceExtras/src/org/evolution/oneplus/DeviceExtras/Startup.java
@@ -43,7 +43,6 @@ public class Startup extends BroadcastReceiver {
         org.evolution.oneplus.DeviceExtras.doze.DozeUtils.checkDozeService(context);
         EarGainPreference.restore(context);
         MicGainPreference.restore(context);
-        org.evolution.oneplus.DeviceExtras.touch.TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
         VibratorCallStrengthPreference.restore(context);
         VibratorNotifStrengthPreference.restore(context);
         VibratorStrengthPreference.restore(context);
